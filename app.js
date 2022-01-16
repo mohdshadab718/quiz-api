89c2fb762debf2b7783002a13eebcd1f8941a6bf
Mohd Shadab <mohdshadab0718@gmail.com>
2022-01-16 14:21:23 +0530

    installed dotenv

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3100;
+const dotenv = require('dotenv').config();
 
 app.get('/', (req, res)=>{
     res.send("Home Page");
 })
 
 
-app.listen(PORT, p=> console.log("Server started onn port", p))
+app.listen(process.env.PORT, p=> console.log("Server started onn port", process.env.PORT))
