770894e8fd99134592244c8aabc687ceca7b7d87
Mohd Shadab <mohdshadab0718@gmail.com>
2022-01-16 14:07:15 +0530

    app.js modified by shadab

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3100;
+
+app.get('/', (req, res)=>{
+    res.send("Home Page");
+})
+
+app.listen(PORT, p=> console.log("Server started onn port", p))
